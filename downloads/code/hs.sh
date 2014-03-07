95bbd433b18f9389dddb67f4a345302579c4ed35
Yanwei Yao <yanwei.yao@me.com>
2014-03-07 14:21:55 +0800

    Site updated at 2014-03-07 06:21:55 UTC

---
+++ downloads/code/hs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: hs.sh
+# 
+#         USAGE: ./hs.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 02/07/14 13:41
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo "Hello BASH"
+
